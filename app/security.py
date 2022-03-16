aeec9f00db7860c95e46d8aca3d49d1b5d71e0d4
zebra <zebra_cl@foxmail.com>
2022-03-16 14:38:40 +0800

    username

--- app/security.py
+++ app/security.py
@@ -32,7 +32,7 @@ class SecurityApiEx(SecurityApi):
   @safe
   def userinfo(self):
     class _Schema(Schema):
-      username = fields.String(attribute='name')
+      name = fields.String(attribute='username')
       avatar = fields.String(
         default='https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif')
       roles = fields.List(fields.String())
